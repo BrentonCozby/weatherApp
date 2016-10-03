1424bd94652e7b49698d8a4e88a431e02648647c
Brenton <brenton.cozby@gmail.com>
2016-10-03 19:24:16 -0400

    update js file to match codepen

--- js/main.js
+++ js/main.js
@@ -1,64 +1,92 @@
 $(document).ready(function() {
 
-	// get local zip code from ip address
-	$.ajax({
-    url: "http://ipinfo.io/json",
-    success: function(res, txt, xhr) {
-      console.log(res);
-      $("#city").html(res.city);
-      $("#country").html(res.country);
-			document.getElementById("chooseZip").placeholder = res.postal;
+    $(".main-container").fadeIn(1200);
 
-			function getWeather(zip) {
-				var apiLink = "";
-				if( $("#chooseZip").val() !== "") {
-					apiLink = "http://api.openweathermap.org/data/2.5/weather?zip=" + $("#chooseZip").val() + ",US&APPID=aebb13fe929de5b1d6b3355ef5dc181c";
-				}
-				else {
-					apiLink = "http://api.openweathermap.org/data/2.5/weather?zip=" + zip + ",US&APPID=aebb13fe929de5b1d6b3355ef5dc181c";
-				}
+    // get local zip code from ip address
+    $.ajax({
+        url: "http://ipinfo.io/json",
+        success: function(res, txt, xhr) {
+            console.log(res);
+            $("#city").html(res.city);
+            document.getElementById("chooseZip").placeholder = res.postal;
 
-				$.ajax({
-	        url: apiLink,
-	        success: function(res2, txt2, xhr2) {
-	          console.log(res2);
-						$("#city").html(res2.name);
-	          $("#temp").html( (res2.main.temp * (9/5) - 459.67).toFixed(0) + " &deg;F");
-						$("#weather").html(res2.weather[0].main);
-	          $("#humidity").html(res2.main.humidity + "%");
+            function getWeather(zip) {
+                var apiLink = "";
+                if ($("#chooseZip").val() !== "") {
+                    apiLink = "http://api.openweathermap.org/data/2.5/weather?zip=" + $("#chooseZip").val() + ",US&APPID=aebb13fe929de5b1d6b3355ef5dc181c";
+                } else {
+                    apiLink = "http://api.openweathermap.org/data/2.5/weather?zip=" + zip + ",US&APPID=aebb13fe929de5b1d6b3355ef5dc181c";
+                }
 
-						function getTime(timestamp) {
-							var date = new Date(timestamp*1000);
-							// Hours part from the timestamp
-							var hours = date.getHours();
-							if(hours > 12) hours -= 12;
-							// Minutes part from the timestamp
-							var minutes = "0" + date.getMinutes();
-							// Will display time in 10:30:23 format
-							var formattedTime = hours + ':' + minutes.substr(-2);
+                $.ajax({
+                    url: apiLink,
+                    success: function(res2, txt2, xhr2) {
+                        console.log(res2);
+                        $("#city").html(res2.name + ",&nbsp;");
+                        $("#country").html(res2.sys.country)
+                        $("#temp").html((res2.main.temp * (9 / 5) - 459.67).toFixed(0));
+						$("#temp-change").html("°F");
+						$("#temp-change").unbind().on("click", function() {
+							console.log($("#temp-change").html());
+							if($("#temp-change").html() == "°F") {
+								$("#temp").html((res2.main.temp - 273.15).toFixed(0))
+								$("#temp-change").html("°C");
+							}
+							else {
+								$("#temp").html((res2.main.temp * (9 / 5) - 459.67).toFixed(0))
+								$("#temp-change").html("°F");
+							}
 
-							return formattedTime;
+						})
+                        $("#weather").html(res2.weather[0].description);
+						if(res2.weather[0].id < 300) {
+							$(".data img").attr("src", "http://i.imgur.com/G5dIJDd.png");
+						} else if(res2.weather[0].id < 600) {
+							$(".data img").attr("src", "http://i.imgur.com/V8jVgVf.png");
+						} else if(res2.weather[0].id < 700) {
+							$(".data img").attr("src", "http://i.imgur.com/bl5B3Sg.png");
+						} else if(res2.weather[0].id === 800) {
+							$(".data img").attr("src", "http://i.imgur.com/u3SmnRQ.png");
+						} else if(res2.weather[0].id === 801) {
+							$(".data img").attr("src", "http://i.imgur.com/Q7kUCgj.png");
+						} else if(res2.weather[0].id < 900) {
+							$(".data img").attr("src", "http://i.imgur.com/SxaehkV.png");
 						}
+                        $("#humidity").html(res2.main.humidity + "%");
 
-	          $("#sunrise").html( getTime(res2.sys.sunrise) + " AM");
-	          $("#sunset").html( getTime(res2.sys.sunset) + " PM");
+                        function getTime(timestamp) {
+                            var date = new Date(timestamp * 1000);
+                            // Hours part from the timestamp
+                            var hours = date.getHours();
+                            if (hours > 12) hours -= 12;
+                            // Minutes part from the timestamp
+                            var minutes = "0" + date.getMinutes();
+                            // Will display time in 10:30:23 format
+                            var formattedTime = hours + ':' + minutes.substr(-2);
 
-						$(".main-container").fadeIn('fast');
-	        }
-	      });
-			}
+                            return formattedTime;
+                        }
 
-			getWeather(res.postal);
+                        $("#sunrise").html(getTime(res2.sys.sunrise) + " AM");
+                        $("#sunset").html(getTime(res2.sys.sunset) + " PM");
 
-			$("#chooseZip").change(function() {
-				$(".main-container").fadeOut('fast', function() {
-					getWeather( $("#chooseZip").val() );
-				});
+                        $(".main-container").fadeIn('fast');
+                        $("#chooseZip").focus().select();
+                    }
+                });
+            }
 
-			});
+            getWeather(res.postal);
+
+            $("#chooseZip").change(function() {
+                $(".main-container").fadeOut('fast', function() {
+                    getWeather($("#chooseZip").val());
+                });
+
+            });
 
 
-    }
-  });
+        }
+    });
 
 });
